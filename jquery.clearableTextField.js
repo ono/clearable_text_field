a39d74cac99937e5d0dd206c9a001f3da215adb3
Tatsuya Ono <ononoma@gmail.com>
2009-11-18 23:28:14 +0000

    Created mini version. Releases version 0.2

--- jquery.clearableTextField.js
+++ jquery.clearableTextField.js
@@ -1,5 +1,5 @@
  /*
-  * Clearable Text Field - jQuery plugin version 0.1
+  * Clearable Text Field - jQuery plugin version 0.2
   * Copyright (c) 2009 Tatsuya Ono
   *
   * http://github.com/ono/clearable_text_field
